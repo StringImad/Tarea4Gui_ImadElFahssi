0f18c17176c45bb4c9391a30ea1cdf017657fee9
StringImad <73947304+StringImad@users.noreply.github.com>
2022-05-18 13:39:37 +0200

    Crea proyecto calculador y sube le repositorio desde ies mar de alboran

---
+++ src/main/java/imad/tarea4gui_imadelfahssi_calculadora/Tarea4GUI_ImadELFahssi_Calculadora.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package imad.tarea4gui_imadelfahssi_calculadora;
+
+/**
+ *
+ * @author imad
+ */
+public class Tarea4GUI_ImadELFahssi_Calculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
